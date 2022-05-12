307a5386e914a3e83eabc182d5e9384c0b06229f
aidanliddiard <aidanliddiard@gmail.com>
2022-05-12 16:11:56 -0700

    adding context

--- src/context/ListProvider.jsx
+++ src/context/ListProvider.jsx
@@ -19,3 +19,37 @@ const listReducer = (state, action) => {
       throw new Error(`Action type ${action.type} is not supported`);
   }
 };
+export const ListContext = createContext();
+
+export const ListProvider = ({ children }) => {
+  const [list, dispatch] = useReducer(listReducer, initialList);
+
+  const handleAddItem = (text) => {
+    dispatch({ type: 'ADD_ITEM', payload: { text } });
+  };
+
+  const handleDeleteItem = (id) => {
+    dispatch({ type: 'DELETE_ITEM', payload: { id } });
+  };
+
+  const handleEditItem = (item) => {
+    dispatch({ type: 'EDIT_ITEM', payload: { item } });
+  };
+
+  return (
+    <ListContext.Provider
+      value={{ list, handleAddItem, handleDeleteItem, handleEditItem }}
+    >
+      {children}
+    </ListContext.Provider>
+  );
+};
+
+export const useList = () => {
+  const context = useContext(ListContext);
+
+  if (context === undefined)
+    throw new Error('useList must be called from within a ListProvider');
+
+  return context;
+};
